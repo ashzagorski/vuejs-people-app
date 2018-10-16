ad1aac2a4d60b62de36dd1e94f4401fa64e3442d
ashzagorski <ash.zagorski@gmail.com>
2018-10-15 21:32:23 -0500

    creates people-app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>{{ message }}</h1>
+
+    <div> <h4>Number of People: {{ count }} </h4></div>
+
+    <div v-for="person in people">
+      <h2>Name: {{person.name}} </h2>
+      <p>Bio: {{ person.bio }} </p>
+    </div>
+
+    
+
+    <div>
+      <h4>Create New Person</h4>
+      Name: <input v-model="newPerson.name">
+      Bio: <input v-model="newPerson.bio">
+      <button v-on:click="addPerson()">Add Person</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "People",
+      people: [
+      {name: "Martha Vineyard", bio: "An elegant lady that likes expensive things and wine.", bioVisible: true},
+      {name: "Yeller Longfoot", bio: "From deep south, likes chewing tobacco and his hound dog.", bioVisible: true}],
+
+      newPerson: {name: "", bio: "", bioVisible: true},
+    };
+  },
+  created: function() {},
+  methods: {
+    addPerson: function() {
+      this.people.push(this.newPerson);
+      this.newPerson = {name: "", bio: "", bioVisible: true};
+    },
+    countPeople: function() {
+      var count = this.people.length;
+    }
+  },
+  computed: {}
+};
 </script>
